25089268c79991e45ac1e7fc688d0ef2042e1bf5
aws-joe <fontesj@amazon.com>
2015-08-25 15:41:29 -0700

    shorter name

--- www/index.php
+++ www/index.php
@@ -12,7 +12,7 @@
 # Please see LICENSE.txt for applicable license terms and NOTICE.txt for applicable notices. 
 */
 
-$AppName = "Jenkins Demo Web App";
+$AppName = "Jenkins Demo";
 
 ob_start();
 
